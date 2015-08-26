0099eae82d044bb1214b99c39decba54540322db
Eric Davies <iamed2@gmail.com>
2015-08-26 15:04:18 -0500

    Removed 0.3 compatibility

--- src/DBAPI.jl
+++ src/DBAPI.jl
@@ -2,44 +2,40 @@ module DBAPI
 
 import Base: connect, close
 
-if VERSION < v"0.4.0-dev"
-    using Docile
-end
-
 abstract AbstractDatabaseInterface
 abstract AbstractDatabaseConnection{T<:AbstractDatabaseInterface}
 abstract AbstractDatabaseCursor{T<:AbstractDatabaseInterface}
 abstract AbstractDatabaseError{T<:AbstractDatabaseInterface} <: Exception
 
-@doc """
+"""
 If this error is thrown, a driver has not implemented a required function
 of this interface.
-""" ->
+"""
 type NotImplementedError{T<:AbstractDatabaseInterface} <: AbstractDatabaseError{T} end
 function Base.showerror{T<:AbstractDatabaseInterface}(io::IO, e::NotImplementedError{T})
     print(io, T, " does not implement this required DBAPI feature")
 end
 
-@doc """
+"""
 If this error is thrown, a user has attempted to use an optional function
 of this interface which the driver does not implement.
-""" ->
+"""
 type NotSupportedError{T<:AbstractDatabaseInterface} <: AbstractDatabaseError{T} end
 function Base.showerror{T<:AbstractDatabaseInterface}(io::IO, e::NotSupportedError{T})
     print(io, T, " does not support this optional DBAPI feature")
 end
 
 
-@doc """
+"""
 Constructs a database connection.
 
 Returns `connection::AbstractDatabaseConnection`.
-""" ->
+"""
 function connect{T<:AbstractDatabaseInterface}(::Type{T}, args...; kwargs...)
     throw(NotImplementedError{T}())
 end  # returns T<:AbstractDatabaseConnection
 
-@doc """
+"""
 Close the connection now (rather than when the finalizer is called).
 
 Any further attempted operations on the connection or its cursors will throw a
@@ -49,43 +45,43 @@ Closing a connection without committing will cause an implicit rollback to be
 performed.
 
 Returns `nothing`.
-""" ->
+"""
 function close{T<:AbstractDatabaseInterface}(conn::AbstractDatabaseConnection{T})
     throw(NotImplementedError{T}())
 end
 
-@doc """
+"""
 Commit any pending transaction to the database.
 
 Dataase drivers that do not support transactions should implement this
 function with no body.
 
 Returns `nothing`.
-""" ->
+"""
 function commit{T<:AbstractDatabaseInterface}(conn::AbstractDatabaseConnection{T})
     throw(NotImplementedError{T}())
 end
 
-@doc """
+"""
 Roll back to the start of any pending transaction.
 
 Database drivers that do not support transactions may not implement this
 function.
 
 Returns `nothing`.
-""" ->
+"""
 function rollback{T<:AbstractDatabaseInterface}(conn::AbstractDatabaseConnection{T})
     throw(NotSupportedError{T}())
 end
 
-@doc """
+"""
 Create a new database cursor.
 
 If the database does not implement cursors, the driver must implement a cursor
 object which emulates cursors to the extent required by the interface.
 
 Returns `cursor::AbstractDatabaseCursor`
-""" ->
+"""
 function cursor{T<:AbstractDatabaseInterface}(conn::AbstractDatabaseConnection{T})
     throw(NotImplementedError{T}())
 end
