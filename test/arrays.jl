d390d8a74cfbac4030f9e52337507a59a8c32332
Eric Davies <iamed2@gmail.com>
2015-11-02 14:04:57 -0600

    Nullable tests.

--- test/arrays.jl
+++ test/arrays.jl
@@ -16,7 +16,7 @@ function main()
     # invalid
     @test_throws ArrayInterfaceError Base.connect(ColumnarArrayInterface, [:foo], Vector[])
     @test_throws ArrayInterfaceError Base.connect(ColumnarArrayInterface, Symbol[], Vector[[1, 2, 3]])
-    @test_throws ArrayInterfaceError Base.connect(ColumnarArrayInterface, [:foo], Vector[[1], [2, 3]])
+    @test_throws ArrayInterfaceError Base.connect(ColumnarArrayInterface, [:foo, :bar], Vector[[1], [2, 3]])
 
     # empty
     connection = Base.connect(ColumnarArrayInterface, Symbol[], Vector[])
@@ -28,6 +28,7 @@ function main()
 
     cursor = DBAPI.cursor(connection)
     @test isa(cursor, DBAPI.DatabaseCursor)
+    @test isa(cursor, DBAPI.FixedLengthDatabaseCursor)
 
     @test_throws ArrayInterfaceError DBAPI.rows(cursor)
     @test_throws ArrayInterfaceError DBAPI.columns(cursor)
@@ -66,15 +67,11 @@ function main()
 
     cursor = DBAPI.cursor(connection)
     @test isa(cursor, DBAPI.DatabaseCursor)
+    @test isa(cursor, DBAPI.FixedLengthDatabaseCursor)
 
     @test_throws DBAPI.DatabaseQueryError DBAPI.execute!(cursor, ColumnarArrayQuery([:one], 1:1))
 
-    try
-        DBAPI.execute!(cursor, ColumnarArrayQuery(Symbol[], 1:0))
-    catch error
-        println(error)
-        rethrow(error)
-    end
+    @test DBAPI.execute!(cursor, ColumnarArrayQuery(Symbol[], 1:0)) === nothing
 
     @test isempty(collect(DBAPI.rows(cursor)))
     @test isempty(collect(DBAPI.columns(cursor)))
@@ -157,7 +154,7 @@ function main()
 
     for ds in empty_data_structures
         # ds will be a collection of an empty row
-        @test (ds, 1) === DBAPI.fetchintorows!(ds, cursor)
+        @test (ds, 1) == DBAPI.fetchintorows!(ds, cursor)
     end
 
     for ds in empty_data_structures
@@ -229,6 +226,45 @@ function main()
             @test length(ds) == 1
         end
     end
+
+    # nullable db data
+    connection = Base.connect(
+        ColumnarArrayInterface,
+        [:foo, :bar],
+        Vector[[Nullable(1), Nullable(2), Nullable(3)], [3.0, 2.0, 1.0]]
+    )
+    @test isa(connection, DBAPI.DatabaseConnection)
+    @test DBAPI.isopen(connection)
+    @test DBAPI.commit(connection) === nothing
+    @test_throws DBAPI.NotSupportedError DBAPI.rollback(connection)
+    @test Base.isopen(connection)
+
+    cursor = DBAPI.cursor(connection)
+    @test isa(cursor, DBAPI.DatabaseCursor)
+    @test isa(cursor, DBAPI.FixedLengthDatabaseCursor)
+
+    @test_throws DBAPI.DatabaseQueryError DBAPI.execute!(cursor, ColumnarArrayQuery([:one], 1:1))
+
+    @test DBAPI.execute!(cursor, ColumnarArrayQuery(Symbol[], 1:0)) === nothing
+
+    @test isempty(collect(DBAPI.rows(cursor)))
+    @test isempty(collect(DBAPI.columns(cursor)))
+    @test isempty(cursor)
+    @test length(cursor) == 0
+    @test_throws BoundsError cursor[1, 1]
+    @test_throws BoundsError cursor[1, :one]
+
+    @test DBAPI.execute!(cursor, ColumnarArrayQuery([:foo, :bar], 2:3)) === nothing
+    row_results = Any[
+        (Nullable(2), 2.0),
+        (Nullable(3), 1.0),
+    ]
+    nullable_row_results = map(x -> map(y -> isa(y, Nullable) ? y : Nullable{Any}(y), x), row_results)
+    @test isequal(collect(DBAPI.rows(cursor)), row_results)
+    @test isequal(collect(DBAPI.columns(cursor)), Vector[
+        [Nullable(2), Nullable(3)],
+        [2.0, 1.0],
+    ])
 
     # bad queries
     @test_throws DBAPI.NotImplementedError DBAPI.execute!(
